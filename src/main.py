9966a414a670c11a314948c11fdc90d0d6833820
bfourk <100809777+bfourk@users.noreply.github.com>
2024-11-08 20:26:16 -0500

    Revert "Remove unnecessary variable"
    Did not read the code properly, it is a required varible.
    This reverts commit 094bf66f05acb84f7bade910098b081f508745cf.

--- src/main.py
+++ src/main.py
@@ -1,13 +1,15 @@
 from sys import argv
 
-server = False # If we run in server mode (set below)
+server = False # if we run in server mode (set by the below for loop)
 passwd = None
 
+inc = 0
 for arg in argv:
 	if arg == "--server" or arg == "-s":
 		server = True
 	if arg == "--passwd" and len(argv) >= inc:
 		passwd = argv[inc+1]
+	inc += 1
 
 if server:
 	from rserver import run
